56176319c54f08f61d533b66e2dcf6e2dd7fbb65
BannerBomb <BannerBomb55@gmail.com>
2020-03-04 02:28:17 -0500

    Update ReactionHandler.js

--- src/lib/util/ReactionHandler.js
+++ src/lib/util/ReactionHandler.js
@@ -330,7 +330,7 @@ class ReactionHandler extends ReactionCollector {
 	 */
 	async _queueEmojiReactions(emojis) {
 		if (this.message.deleted) return this.stop();
-		if (this.ended) return this.message.reactions.remove();
+		if (this.ended) return this.message.reactions.removeAll();
 		await this.message.react(emojis.shift());
 		if (emojis.length) return this._queueEmojiReactions(emojis);
 		this.reactionsDone = true;
